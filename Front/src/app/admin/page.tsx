a3e66693f416b49dab5e438ebf0510e2a61a7d31
siskoweb <sisko4dev@gmail.com>
2023-07-20 16:20:04 +0200

    editing sidebar

--- Front/src/app/admin/page.tsx
+++ Front/src/app/admin/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+import Status from "../../components/AdminDashboard/utilis/Status";
 export default function AdminPage() {
-  return <div>AdminPage</div>
+  return (
+    <div>
+      <Status />
+      AdminPage
+    </div>
+  );
 }
