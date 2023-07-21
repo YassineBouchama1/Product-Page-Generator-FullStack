c83e5c71a25da90563d9d6383fd624e37761ed29
siskoweb <sisko4dev@gmail.com>
2023-07-21 17:43:46 +0200

    edit product card

--- Front/src/components/AdminDashboard/ProductCard.tsx
+++ Front/src/components/AdminDashboard/ProductCard.tsx
@@ -15,23 +15,23 @@ export default function ProductCard() {
           className="w-24 h-24 mb-3 rounded-full shadow-lg"
         />{" "}
         <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-          Bonnie Green
+          Product Name{" "}
         </h5>
         <span className="text-sm text-gray-500 dark:text-gray-400">
-          Visual Designer
+          Quantity:23
         </span>
         <div className="flex mt-4 space-x-3 md:mt-6 gap-2">
           <Link
             href="#"
-            className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="inline-flex items-center px-4 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Add friend
+            Delete
           </Link>
           <Link
             href="#"
             className="inline-flex items-center px-4  text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700"
           >
-            Message
+            Edit
           </Link>
         </div>
       </div>
